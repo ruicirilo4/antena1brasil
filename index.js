4caaa65258c8dfc9104500ab87e228d7f6aedd01
ruicirilo4 <100714573+ruicirilo4@users.noreply.github.com>
2023-09-20 16:15:32 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,52 +1,46 @@
-const puppeteer = require("puppeteer");
-const express = require("express");
-const { Browserless } = require("browserless"); // Import the browserless package
+import express from 'express';
+import puppeteer from 'puppeteer';
+import puppeteerCore from 'puppeteer-core';
+import { addExtra } from 'puppeteer-extra';
+import puppeteerStealth from 'puppeteer-extra-plugin-stealth';
+
 const app = express();
 const port = 56189;
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Create an array to store recent songs
 const recentSongs = [];
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+// Use stealth plugin to make Puppeteer more stealthy
+const browser = await puppeteer.launch({
+  executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', // Specify the path to Chromium or Chrome executable
+  headless: true, // Set to true for running in headless mode
+  args: ['--no-sandbox', '--disable-setuid-sandbox'],
+});
+const page = await browser.newPage();
+await page.goto('https://www.antena1.com.br/ouvir-radio-online');
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: process.cwd() });
 });
 
-app.get("/now-playing", async (req, res) => {
+app.get('/now-playing', async (req, res) => {
   try {
-    // Create a new browserless instance with your API token
-    const browserless = new Browserless({
-      token: process.env.BROWSERLESS_TOKEN, // Use your browserless API token
-    });
+    const artistSelector = '.Artist';
+    const songSelector = '.nowPlayingLink';
 
-    const artistSelector = ".Artist";
-    const songSelector = ".nowPlayingLink";
-
-    // Use browserless to open a new page
-    const page = await browserless.page();
-
-    // Navigate to the URL
-    await page.goto("https://www.antena1.com.br/ouvir-radio-online");
-
-    // Wait for the selectors to appear on the page
     await page.waitForSelector(artistSelector, { timeout: 180000 });
     await page.waitForSelector(songSelector);
 
-    // Extract artist and song text
-    const [artistText, songText] = await Promise.all([
-      page.textContent(artistSelector),
-      page.textContent(songSelector),
-    ]);
+    const artistText = await page.$eval(artistSelector, (element) => element.textContent.trim());
+    const songText = await page.$eval(songSelector, (element) => element.textContent.trim());
 
-    // Close the page
-    await page.close();
-
-    const nowPlayingInfo = `${artistText.trim()} - ${songText.trim()}`;
+    const nowPlayingInfo = `${artistText} - ${songText}`.trim();
 
     // Check if the song is not already in the recentSongs array,
     // and it's not "-" or an empty string before adding it
-    if (nowPlayingInfo !== "-" && nowPlayingInfo !== "" && !recentSongs.includes(nowPlayingInfo)) {
+    if (nowPlayingInfo !== '-' && nowPlayingInfo !== '' && !recentSongs.includes(nowPlayingInfo)) {
       // Add the currently playing song to the beginning of the recentSongs array
       recentSongs.unshift(nowPlayingInfo);
 
@@ -58,14 +52,14 @@ app.get("/now-playing", async (req, res) => {
 
     res.send(nowPlayingInfo);
   } catch (error) {
-    console.error("Error:", error);
-    res.status(500).send("Error");
+    console.error('Error:', error);
+    res.status(500).send('Error');
   }
 });
 
-app.get("/recent-songs", (req, res) => {
+app.get('/recent-songs', (req, res) => {
   // Filter out "-" and empty string from the recentSongs array
-  const filteredRecentSongs = recentSongs.filter((song) => song !== "-" && song !== "");
+  const filteredRecentSongs = recentSongs.filter((song) => song !== '-' && song !== '');
 
   // Return the filtered list of recent songs
   res.json(filteredRecentSongs);
@@ -74,3 +68,4 @@ app.get("/recent-songs", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
