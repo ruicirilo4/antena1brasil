79a2dd956b36857fa5fad12cd2edc42e42112e6b
ruicirilo4 <100714573+ruicirilo4@users.noreply.github.com>
2023-09-20 16:33:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,20 @@ app.use(express.static('public'));
 // Create an array to store recent songs
 const recentSongs = [];
 
-// Use stealth plugin to make Puppeteer more stealthy
-const browser = await puppeteer.launch({
-  executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', // Specify the path to Chromium or Chrome executable
-  headless: true, // Set to true for running in headless mode
-  args: ['--no-sandbox', '--disable-setuid-sandbox'],
-});
-const page = await browser.newPage();
-await page.goto('https://www.antena1.com.br/ouvir-radio-online');
+async function setupPuppeteer() {
+  const puppeteerOptions = {
+    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', // Specify the path to Chromium or Chrome executable
+    headless: true, // Set to true for running in headless mode
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  };
+
+  // Use stealth plugin to make Puppeteer more stealthy
+  const browser = await puppeteer.launch(puppeteerOptions);
+  const page = await browser.newPage();
+  await page.goto('https://www.antena1.com.br/ouvir-radio-online');
+
+  return { browser, page };
+}
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: process.cwd() });
@@ -27,6 +33,8 @@ app.get('/', (req, res) => {
 
 app.get('/now-playing', async (req, res) => {
   try {
+    const { browser, page } = await setupPuppeteer();
+
     const artistSelector = '.Artist';
     const songSelector = '.nowPlayingLink';
 
@@ -49,6 +57,28 @@ app.get('/now-playing', async (req, res) => {
         recentSongs.pop(); // Remove the oldest song if there are more than 10
       }
     }
+
+    // Close the browser after each request to save resources
+    await browser.close();
+
+    res.send(nowPlayingInfo);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Error');
+  }
+});
+
+app.get('/recent-songs', (req, res) => {
+  // Filter out "-" and empty string from the recentSongs array
+  const filteredRecentSongs = recentSongs.filter((song) => song !== '-' && song !== '');
+
+  // Return the filtered list of recent songs
+  res.json(filteredRecentSongs);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
     res.send(nowPlayingInfo);
   } catch (error) {
